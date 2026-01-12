f0784490705e73fc4e18452d60cda8b8b6c16b7d
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-01-12 16:04:08 +0100

    fixed imports in cntracts

--- frontend/constants/contracts.ts
+++ frontend/constants/contracts.ts
@@ -1,35 +1,20 @@
 // constants/contracts.ts
 import { Address } from 'viem';
-import { celo, base, polygon } from 'wagmi/chains'; // import your chains
+import { mantle } from 'wagmi/chains'; // import your chains
 
 export const TYCOON_CONTRACT_ADDRESSES: Record<number, Address | undefined> = {
-  [celo.id]: process.env.NEXT_PUBLIC_CELO as Address,
-  [base.id]: process.env.NEXT_PUBLIC_BASE as Address,
-//   [polygon.id]: process.env.NEXT_PUBLIC_CELO as Address,
-  // Add more chains as needed
-  // If not deployed → leave undefined
+  [mantle.id]: process.env.NEXT_PUBLIC_MANTLE as Address,
+ 
 };
 export const REWARD_CONTRACT_ADDRESSES: Record<number, Address | undefined> = {
-  [celo.id]: process.env.NEXT_PUBLIC_CELO_REWARD as Address,
-  [base.id]: process.env.NEXT_PUBLIC_BASE_REWARD as Address,
-//   [polygon.id]: process.env.NEXT_PUBLIC_CELO as Address,
-  // Add more chains as needed
-  // If not deployed → leave undefined
+  [mantle.id]: process.env.NEXT_PUBLIC_MANTLE_REWARD as Address,
 }
 export const TYC_TOKEN_ADDRESS: Record<number, Address | undefined> = {
-  [celo.id]: process.env.NEXT_PUBLIC_CELO_REWARD as Address,
-  [base.id]: process.env.NEXT_PUBLIC_BASE_TOKEN as Address,
-//   [polygon.id]: process.env.NEXT_PUBLIC_CELO as Address,
-  // Add more chains as needed
-  // If not deployed → leave undefined
+  [mantle.id]: process.env.NEXT_PUBLIC_MANTLE_REWARD as Address,
 }
 
 export const USDC_TOKEN_ADDRESS: Record<number, Address | undefined> = {
-  [celo.id]: process.env.NEXT_PUBLIC_CELO_USDC as Address,
-  [base.id]: process.env.NEXT_PUBLIC_BASE_USDC as Address,
-//   [polygon.id]: process.env.NEXT_PUBLIC_CELO as Address,
-  // Add more chains as needed
-  // If not deployed → leave undefined
+  [mantle.id]: process.env.NEXT_PUBLIC_MANTLE_USDC as Address,
 }
 // constants/contracts.ts
 export const MINIPAY_CHAIN_IDS = [42220]; // Celo Mainnet & Alfajores
