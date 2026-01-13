39169225692ebcbd04dcdbbd4f59c1283399e84d
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-01-13 14:26:43 +0100

    chore: added mantleSepoliaTestnet

--- frontend/constants/contracts.ts
+++ frontend/constants/contracts.ts
@@ -13,7 +13,7 @@ export const REWARD_CONTRACT_ADDRESSES: Record<number, Address | undefined> = {
 }
 export const TYC_TOKEN_ADDRESS: Record<number, Address | undefined> = {
   // [mantle.id]: process.env.NEXT_PUBLIC_MANTLE_REWARD as Address,
-  [mantleSepoliaTestnet.id]: process.env.NEXT_PUBLIC_MANTLE_REWARD_SEPOLIA as Address,
+  [mantleSepoliaTestnet.id]: process.env.NEXT_PUBLIC_MANTLE_USDC_SEPOLIA as Address,
 }
 
 export const USDC_TOKEN_ADDRESS: Record<number, Address | undefined> = {
